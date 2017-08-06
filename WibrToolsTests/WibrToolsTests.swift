2752d3acc39afa9813becdd0aaae055f466b8527
wibr <wibr@icloud.com>
2017-08-06 18:22:22 +0200

    Spiral struct + tests added

---
+++ WibrToolsTests/WibrToolsTests.swift
@@ -0,0 +1,44 @@
+//
+//  WibrToolsTests.swift
+//  WibrToolsTests
+//
+//  Created by Winfried Brinkhuis on 06-08-17.
+//
+@testable import WibrTools
+import XCTest
+
+class WibrToolsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testUlamSpiralPosition() {
+        let ulam = Spiral.Ulam()
+        let number = 888
+        let expectedPosition = (ring:31, x:-2, y:15)
+        let calculatedPosition = ulam.calculatePosition(num: number)
+        let corner  = Spiral.Ulam.isCorner((calculatedPosition.x,calculatedPosition.y))
+        XCTAssertFalse(corner)
+        XCTAssertEqual(calculatedPosition.ring, expectedPosition.ring)
+        XCTAssertEqual(calculatedPosition.x, expectedPosition.x)
+        XCTAssertEqual(calculatedPosition.y, expectedPosition.y)
+    }
+    
+    func testUlamSumOfCorners() {
+        let size = 1001
+        let nums = size * size
+        let expectedSum = 101
+        let actualSum = Spiral.UlamSequence().prefix(nums).filter{ abs($0.x) == abs($0.y) }.reduce(0) { $0 + $1.num }
+        print(actualSum)
+//        XCTAssertTrue(actualSum == expectedSum)
+        
+    }
+    
+}
